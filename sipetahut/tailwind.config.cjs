05328abc3032ff2e39878ddb7341a8b4386cbc27
theofanihati <theofanihati@gmail.com>
2025-07-07 17:42:21 +0700

    feat(login): slicing UI login + fix(tailwind): downgrading to tailwind 3 due to incompatible with postcss

--- sipetahut/tailwind.config.cjs
+++ sipetahut/tailwind.config.cjs
@@ -1,14 +1,9 @@
 module.exports = {
-  purge: [],
-  darkMode: false,
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
